6537f2ac54666c1f3626b0d8295eba1680c4864d
davidmcgregor <davidjohnmcgregor@gmail.com>
2020-03-09 19:57:26 +0000

    Refactor

--- lib/TenMinutesWalk.rb
+++ lib/TenMinutesWalk.rb
@@ -1,27 +1,5 @@
 class App
   def tenminuteswalk(arr)
-
-    if arr.length != 10
-      return false
-    end
-    
-    direction_counter = 0
-
-    arr.each { |letter|
-      if letter === 'n'
-        direction_counter += 1
-      elsif
-        letter === 'e'
-        direction_counter += 1
-      else
-        direction_counter -= 1
-      end
-    }
-
-    if direction_counter === 0
-      return true
-    else
-      return false
-    end
+    (arr.count("n") == arr.count("s") && arr.count("e") == arr.count("w")) && arr.length == 10 ? true : false
   end
 end
